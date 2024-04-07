f124c0e6f4bc0d34684d556c06dbd74cd38e5186
Raghav <aroramadhav65@gmail.com>
2024-04-07 13:09:14 +0530

    update

--- lib/features/admin/screens/posts_screen.dart
+++ lib/features/admin/screens/posts_screen.dart
@@ -1,8 +1,8 @@
-import 'package:amazon_clone_tutorial/common/widgets/loader.dart';
-import 'package:amazon_clone_tutorial/features/account/widgets/single_product.dart';
-import 'package:amazon_clone_tutorial/features/admin/screens/add_product_screen.dart';
-import 'package:amazon_clone_tutorial/features/admin/services/admin_services.dart';
-import 'package:amazon_clone_tutorial/models/product.dart';
+import 'package:style_sphere/common/widgets/loader.dart';
+import 'package:style_sphere/features/account/widgets/single_product.dart';
+import 'package:style_sphere/features/admin/screens/add_product_screen.dart';
+import 'package:style_sphere/features/admin/services/admin_services.dart';
+import 'package:style_sphere/models/product.dart';
 import 'package:flutter/material.dart';
 
 class PostsScreen extends StatefulWidget {
