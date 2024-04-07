e32510cb7fa6545a05350d5710170a6a6c9df6a6
Raghav <aroramadhav65@gmail.com>
2024-04-07 13:05:00 +0530

    update

--- lib/features/account/widgets/orders.dart
+++ lib/features/account/widgets/orders.dart
@@ -1,9 +1,9 @@
-import 'package:amazon_clone_tutorial/common/widgets/loader.dart';
-import 'package:amazon_clone_tutorial/constants/global_variables.dart';
-import 'package:amazon_clone_tutorial/features/account/services/account_services.dart';
-import 'package:amazon_clone_tutorial/features/account/widgets/single_product.dart';
-import 'package:amazon_clone_tutorial/features/order_details/screens/order_details.dart';
-import 'package:amazon_clone_tutorial/models/order.dart';
+import 'package:style_sphere/common/widgets/loader.dart';
+import 'package:style_sphere/constants/global_variables.dart';
+import 'package:style_sphere/features/account/services/account_services.dart';
+import 'package:style_sphere/features/account/widgets/single_product.dart';
+import 'package:style_sphere/features/order_details/screens/order_details.dart';
+import 'package:style_sphere/models/order.dart';
 import 'package:flutter/material.dart';
 
 class Orders extends StatefulWidget {
