9c2529cf50dd448b97fd615e629dc75e6db8c5d6
Raghav <aroramadhav65@gmail.com>
2024-04-07 13:03:57 +0530

    update

--- lib/features/address/screens/address_screen.dart
+++ lib/features/address/screens/address_screen.dart
@@ -1,15 +1,16 @@
-import 'package:amazon_clone_tutorial/constants/utils.dart';
-import 'package:amazon_clone_tutorial/features/address/services/address_services.dart';
+import 'package:style_sphere/constants/utils.dart';
+import 'package:style_sphere/features/address/services/address_services.dart';
 import 'package:flutter/material.dart';
 import 'package:pay/pay.dart';
 import 'package:provider/provider.dart';
-import 'package:amazon_clone_tutorial/common/widgets/custom_textfield.dart';
-import 'package:amazon_clone_tutorial/constants/global_variables.dart';
-import 'package:amazon_clone_tutorial/providers/user_provider.dart';
+import 'package:style_sphere/common/widgets/custom_textfield.dart';
+import 'package:style_sphere/constants/global_variables.dart';
+import 'package:style_sphere/providers/user_provider.dart';
 
 class AddressScreen extends StatefulWidget {
   static const String routeName = '/address';
   final String totalAmount;
+
   const AddressScreen({
     Key? key,
     required this.totalAmount,
