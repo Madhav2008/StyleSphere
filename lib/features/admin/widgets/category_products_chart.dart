58ef24b8546ab7ca97cbce7fde67718255c9bf32
Raghav <aroramadhav65@gmail.com>
2024-04-07 13:10:00 +0530

    update

--- lib/features/admin/widgets/category_products_chart.dart
+++ lib/features/admin/widgets/category_products_chart.dart
@@ -1,9 +1,10 @@
-import 'package:amazon_clone_tutorial/features/admin/models/sales.dart';
+import 'package:style_sphere/features/admin/models/sales.dart';
 import 'package:charts_flutter/flutter.dart' as charts;
 import 'package:flutter/material.dart';
 
 class CategoryProductsChart extends StatelessWidget {
   final List<charts.Series<Sales, String>> seriesList;
+
   const CategoryProductsChart({
     Key? key,
     required this.seriesList,
